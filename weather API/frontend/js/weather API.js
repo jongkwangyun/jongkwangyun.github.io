2becf9e7ac00d81fab1b3b7259354adcf9ec9a0e
jongkwangyun <yjk0319@gmail.com>
2022-12-18 01:35:39 +0900

    weather API 아이콘 배치 및 뷰포트 크기 맞춤

--- weather API/frontend/js/weather API.js
+++ weather API/frontend/js/weather API.js
@@ -1,3 +1,5 @@
+const todayTitle = document.getElementById('todayTitle');
+
 /*
 base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
 API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
@@ -18,8 +20,6 @@ PCP : 1시간 강수량     범주 (1 mm)  8          강수없음
 REH : 습도             %            8
 SNO : 1시간 신적설     범주 (1 cm)  8          적설없음, 1cm미
 */
-
-
 
 let danGi;
 
@@ -45,10 +45,15 @@ let categoryArr = [];
 
 // 문자열 선언
 const TMP = 'TMP';
-const SKY = 'SKY';
-const PTY = 'PTY';
 const POP = 'POP';
 const REH = 'REH';
+const SP = 'SP'
+const SKY = 'SKY';
+const PTY = 'PTY';
+let SKYorPTY = '';
+const GOTOWORK = 'goToWork';
+const OFFWORK = 'offWork';
+const NOW = 'now';
 
 // 기상청 API '단기예보조회' 주소
 let openApiUrl = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?'
@@ -82,37 +87,86 @@ document.body.onload = () => {
 
 // 기상청 API 응답 받았을때 실행
 function handleResponse() {
-  console.log(getTemp(yesterdayY4MMDD, 'now'));
-
+  console.log(getValue(SP, yesterdayY4MMDD, GOTOWORK));  // 온도: TMP, 강수확률: POP, 습도: REH, 하늘 및 강수: SP (하늘상태: SKY, 강수형태: PTY)
+  console.log(SKYorPTY)
 }
 
-// 온도 얻기
-function getTemp(whichdate, goOff) {
+// 값 얻기
+function getValue(category, whichdate, goOff) {  // 온도: TMP, 강수확률: POP, 습도: REH, 하늘 및 강수: SP (하늘상태: SKY, 강수형태: PTY)
 
-  getCategoryArr('POP');  // 온도: TMP, 하늘상태: SKY, 강수형태: PTY, 강수확률: POP, 습도: REH
+  getCategoryDateArr(category, whichdate, goOff);
+  const returnVal = getReturnVal(category, goOff);
+
+  return returnVal;
+}
+
+function getCategoryDateArr(category, whichdate, goOff) {
+  // 배열을 카테고리별 가공 후 날짜별 가공
+  getCategoryArr(category);
   getWhichdateArr(whichdate);
 
-  switch (goOff) {
-    case 'go':
-      return Math.round((parseInt(categoryArr[7].fcstValue) + parseInt(categoryArr[8].fcstValue)) / 2);  // parseInt(시간항목.값)
-      break;
-    case 'off':
-      return Math.round((parseInt(categoryArr[17].fcstValue) + parseInt(categoryArr[18].fcstValue)) / 2);
-      break;
-    case 'now':
-      return parseInt(categoryArr[timeToday].fcstValue);
-      break;
-    default:
-      return -999;
+  if (category = SP) {  // SP 일때만 검사
+    // 출/퇴근 강수 확인
+    if (goOff == GOTOWORK) {  // 출근 시간 강수 확인  
+      if (categoryArr[7].fcstValue == '0' && categoryArr[8].fcstValue == '0') {
+        SKYorPTY = SKY;  // 강수 없음
+        getCategoryArr(SKY);
+        getWhichdateArr(whichdate);
+      }
+    } else if (goOff == OFFWORK) {  // 퇴근 시간 강수 확인
+      if (categoryArr[17].fcstValue == '0' && categoryArr[18].fcstValue == '0') {
+        SKYorPTY = SKY;
+        getCategoryArr(SKY);
+        getWhichdateArr(whichdate);
+      }
+    }
   }
 }
 
 function getCategoryArr(category) {
   const danGiArr = danGi.response.body.items.item;  // item 객체 모음 배열
-  categoryArr = danGiArr.filter(v => v.category == category);  // category 배열을 필터로 생성
+
+  if (category == SP) {
+    SKYorPTY = PTY;
+    // 강수형태(PTY) 지정 날짜 데이터 배열 만든다.
+    categoryArr = danGiArr.filter(v => v.category == PTY);  // category 배열을 필터로 생성
+
+    // 두 개 시간 중 숫자 더 큰걸로 표시한다.
+  } else {
+    categoryArr = danGiArr.filter(v => v.category == category);  // category 배열을 필터로 생성
+  }
 }
 
 function getWhichdateArr(whichdate) {
   categoryArr = categoryArr.filter(v => v.fcstDate == whichdate)
   return categoryArr;
+}
+
+function getReturnVal(category, goOff) {
+  if (category == TMP || category == POP || category == REH) {  // 온도, 강수확률, 습도 리턴
+    switch (goOff) {
+      case GOTOWORK:
+        return Math.round((parseInt(categoryArr[7].fcstValue) + parseInt(categoryArr[8].fcstValue)) / 2);  // parseInt(시간항목.값)
+        break;
+      case OFFWORK:
+        return Math.round((parseInt(categoryArr[17].fcstValue) + parseInt(categoryArr[18].fcstValue)) / 2);
+        break;
+      case NOW:
+        return parseInt(categoryArr[timeToday].fcstValue);
+        break;
+      default:
+        return -999;
+    }
+  } else if (category == SP) {  // 하늘 및 강수 리턴
+    if (goOff == GOTOWORK) {
+      return parseInt(categoryArr[7].fcstValue) > parseInt(categoryArr[8].fcstValue) ? parseInt(categoryArr[7].fcstValue) : parseInt(categoryArr[8].fcstValue);
+    } else if (goOff == OFFWORK) {
+      return parseInt(categoryArr[17].fcstValue) > parseInt(categoryArr[18].fcstValue) ? parseInt(categoryArr[17].fcstValue) : parseInt(categoryArr[18].fcstValue);
+    } else {
+      return -888;
+    }
+  } else {
+    console.log(category);
+    return -777;
+  }
 }
