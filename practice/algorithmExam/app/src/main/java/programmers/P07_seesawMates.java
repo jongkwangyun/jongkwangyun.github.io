fb82cc9540396ff8a3da2217c2c427fcd6290ef6
jongkwangyun <yjk0319@gmail.com>
2023-01-26 20:58:51 +0900

    시소 문제

--- practice/algorithmExam/app/src/main/java/programmers/P07_seesawMates.java
+++ practice/algorithmExam/app/src/main/java/programmers/P07_seesawMates.java
@@ -20,25 +20,30 @@ public class P07_seesawMates {
 
       for (int p1Idx = 0; p1Idx < total; p1Idx++) {
         for (int p2Idx = p1Idx + 1; p2Idx < total; p2Idx++) {
-          if (weights[p1Idx] == weights[p2Idx]
-              || weights[p1Idx] * 3 == weights[p2Idx] << 2
-              || weights[p1Idx] == weights[p2Idx] << 2
-              || weights[p1Idx] << 2 == weights[p2Idx] * 3
-              || weights[p1Idx] << 1 == weights[p2Idx] * 3
-              || weights[p1Idx] << 1 == weights[p2Idx]
-                  || weights[p1Idx] * 3 == weights[p2Idx] << 1) {
-            int bigger, smaller;
-            if (weights[p1Idx] > weights[p2Idx]) {
-              bigger = weights[p1Idx];
-              smaller = weights[p2Idx];
-            } else {
-              bigger = weights[p2Idx];
-              smaller = weights[p1Idx];
-            }
-            String mates = String.format("%d %d", bigger, smaller);
+
+          if (weights[p1Idx] == weights[p2Idx]) {
+            String mates = String.format("%d %d", weights[p1Idx], weights[p2Idx]);
             matesSet.add(mates);
-            //                System.out.printf("%s\n", mates);
+          } else if (weights[p1Idx] * 3 == weights[p2Idx] << 1) {
+            String mates = String.format("%d %d", weights[p2Idx], weights[p1Idx]);
+            matesSet.add(mates);
+          } else if (weights[p1Idx] == weights[p2Idx] << 1) {
+            String mates = String.format("%d %d", weights[p1Idx], weights[p2Idx]);
+            matesSet.add(mates);
+          } else if (weights[p1Idx] << 2 == weights[p2Idx] * 3) {
+            String mates = String.format("%d %d", weights[p2Idx], weights[p1Idx]);
+            matesSet.add(mates);
+          } else if (weights[p1Idx] << 1 == weights[p2Idx] * 3) {
+            String mates = String.format("%d %d", weights[p1Idx], weights[p2Idx]);
+            matesSet.add(mates);
+          } else if (weights[p1Idx] << 1 == weights[p2Idx]) {
+            String mates = String.format("%d %d", weights[p2Idx], weights[p1Idx]);
+            matesSet.add(mates);
+          } else /*if (weights[p1Idx] * 3 == weights[p2Idx] << 2)*/ {
+            String mates = String.format("%d %d", weights[p1Idx], weights[p2Idx]);
+            matesSet.add(mates);
           }
+          //                System.out.printf("%s\n", mates);
         }
       }
       answer = matesSet.size();
