3599bb1dfb7634300d8f3862879c943eab34831b
jongkwangyun <yjk0319@gmail.com>
2023-01-30 10:49:22 +0900

    깃헙 페이지 반응형 적용

--- practice/algorithmExam/app/src/main/java/practice/Practice.java
+++ practice/algorithmExam/app/src/main/java/practice/Practice.java
@@ -4,6 +4,13 @@ public class Practice {
 
   public static void main(String[] args) {
 
+    enum Direction {
+      EAST, WEST, SOUTH, NORTH
+    }
+
+    System.out.println(Direction.EAST);
+
+
   }
 
 }
