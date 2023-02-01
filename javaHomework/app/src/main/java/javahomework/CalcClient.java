a1d4fecdfb732c8ca3042e83a9ee4edbda62094d
jongkwangyun <yjk0319@gmail.com>
2023-02-01 20:58:14 +0900

    계산기 과제 시작

---
+++ javaHomework/app/src/main/java/javahomework/CalcClient.java
@@ -0,0 +1,10 @@
+package java.homework.app;
+
+public class CalcClient {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
--- practice/algorithmExam/app/src/main/java/practice/Practice.java
+++ practice/algorithmExam/app/src/main/java/practice/Practice.java
@@ -4,13 +4,26 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a = 5;
-    boolean b;
-    while(b = a >= 0) {
-      System.out.println(b);
-      System.out.println(a--);
+    //    String expression = "1 % 2";
+    //
+    //    String[] arr = expression.split("/");
+    //    System.out.println(arr[1]);
+    System.out.println(1.1 / 1 == 1);
+  }
+
+  static boolean isOperator(int codePoint) {
+    final int PLUS = 43;
+    final int MINUS = 45;
+    final int MULTIPLICATION = 42;
+    final int DIVISION = 47;
+    final int MODULO = 37;
+
+    if (codePoint == PLUS || codePoint == MINUS || codePoint == MULTIPLICATION
+        || codePoint == DIVISION || codePoint == MODULO) {
+      return true;
     }
 
+    return false;
   }
 
 }
