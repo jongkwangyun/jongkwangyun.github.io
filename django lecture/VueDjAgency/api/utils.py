d6524c67410f6d37a5ff8716dbb96240b5a55905
jongkwangyun <yjk0319@gmail.com>
2024-01-14 16:55:38 +0900

    django pagination 학습 중

--- django lecture/VueDjAgency/api/utils.py
+++ django lecture/VueDjAgency/api/utils.py
@@ -57,3 +57,17 @@ def prev_next_post(obj):
 
 
     return prevDict, nextDict
+
+
+def obj_to_comment(obj, flag=True):
+    """ comment 객체를 serialize 한다. """
+    comment = dict(vars(obj))
+
+    if obj.update_dt:
+        comment['update_dt'] = obj.update_dt.strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        comment['update_dt'] = '9999-12-31 00:00:00'
+
+    del comment['_state'], comment['post_id'], comment['create_dt']
+
+    return comment
--- django lecture/VueDjAgency/api/views.py
+++ django lecture/VueDjAgency/api/views.py
@@ -1,18 +1,36 @@
 from django.http import JsonResponse
 from django.views import View
 from django.views.generic.detail import BaseDetailView
+from django.views.generic.edit import BaseCreateView
 from django.views.generic.list import BaseListView
 
-from api.utils import obj_to_post, prev_next_post
-from blog.models import Post, Category, Tag
+from api.utils import obj_to_post, prev_next_post, obj_to_comment
+from blog.models import Post, Category, Tag, Comment
 
 
 class ApiPostLV(BaseListView):
-    model = Post
+    # model = Post
+    paginate_by = 3
+
+    def get_queryset(self):
+        paramCate = self.request.GET.get('category')
+        paramTag = self.request.GET.get('tag')
+        if paramCate:
+            qs = Post.objects.filter(category__name__iexact=paramCate)
+        elif paramTag:
+            qs = Post.objects.filter(tags__name__iexact=paramTag)
+        else:
+            qs = Post.objects.all()
+        return qs
 
     def render_to_response(self, context, **response_kwargs):
         qs = context['object_list']
         postList = [obj_to_post(obj, False) for obj in qs]
+
+        pageCnt = context['paginator'].num_pages
+        curPage = context['page_obj'].number
+        print(pageCnt, curPage)
+
         return JsonResponse(data=postList, safe=False, status=200)
 
 
@@ -24,10 +42,14 @@ class ApiPostDV(BaseDetailView):
         post = obj_to_post(obj)
         prevPost, nextPost = prev_next_post(obj)
 
+        qsComment = obj.comment_set.all()
+        commentList = [obj_to_comment(obj) for obj in qsComment]
+
         jsonData = {
             'post': post,
             'prevPost': prevPost,
             'nextPost': nextPost,
+            'commentList': commentList,
         }
         return JsonResponse(data=jsonData, safe=True, status=200)
 
@@ -43,3 +65,26 @@ class ApiCateTagView(View):
             'tagList': tagList,
         }
         return JsonResponse(data=jsonData, safe=True, status=200)
+
+
+class ApiPostLikeDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']
+        obj.like += 1
+        obj.save()
+        return JsonResponse(data=obj.like, safe=False, status=200)
+
+
+class ApiCommentCV(BaseCreateView):
+    model = Comment
+    fields = '__all__'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        comment = obj_to_comment(self.object)
+        return JsonResponse(data=comment, safe=True, status=201)
+
+    def form_invalid(self, form):
+        return JsonResponse(data=form.errors, safe=True, status=400)
